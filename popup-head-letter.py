96ab1a1b8abc6275d9faf446f45cc7e5e54f173d
Hamilkar247 <adonnibal96@gmail.com>
2020-11-04 21:52:13 +0100

    ISS-2 develop QMainWindow in own class MainWindow - window has a title and label

--- popup-head-letter.py
+++ popup-head-letter.py
@@ -1,9 +1,31 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtCore import Qt
+
+
+#Subclass QMainWindow to customize your application's main wind
+class MainWindow(QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.setWindowTitle("Head-letter-Window")
+
+        label = QLabel("Do Wypelnienia")
+
+        # the 'QT' namespace has a lot of attributes to customis 
+        # widgets. See: httpL//doc.qt.io/qt-5/qt.html
+        label.setAlignment(Qt.AlignCenter)
+
+        # Set the central wiget of the Window. Widget will expa
+        # to take up all the space in the window by default
+        self.setCentralWidget(label)
+
+
 
 app = QApplication(sys.argv)
 
-window = QMainWindow()
+window = MainWindow()
 window.show() #IMPORTANT ! - Windows are hidden by default
 
 # Start the event loop
