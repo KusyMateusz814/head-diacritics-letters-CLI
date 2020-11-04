1d6248a65fdd67d2c46a2f8d03da570cabd50fc0
Hamilkar247 <adonnibal96@gmail.com>
2020-11-04 23:24:14 +0100

    ISS-3 show text tips alphabet in python popup

--- popup-head-letter.py
+++ popup-head-letter.py
@@ -1,21 +1,29 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 from PyQt5.QtCore import Qt
-
+from pathlib import Path
 
 #Subclass QMainWindow to customize your application's main wind
 class MainWindow(QMainWindow):
+    headletterStr = ""
 
     def __init__(self, *args, **kwargs):
+        
+        headletterStr = Path('headletterfile.txt').read_text()
+        print(headletterStr)
+
         super(MainWindow, self).__init__(*args, **kwargs)
 
         self.setWindowTitle("Head-letter-Window")
 
-        label = QLabel("Do Wypelnienia")
+        label = QLabel(headletterStr)
 
-        # the 'QT' namespace has a lot of attributes to customis 
+        # the 'QT' namespace has a lot of attributes to customis
         # widgets. See: httpL//doc.qt.io/qt-5/qt.html
-        label.setAlignment(Qt.AlignCenter)
+        #label.setAlignment(Qt.AlignCenter)
+
+        #wrapper text in label
+        label.setWordWrap(True)
 
         # Set the central wiget of the Window. Widget will expa
         # to take up all the space in the window by default
